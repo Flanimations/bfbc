9160265e77db092e68ca76f7e67948e0fcb98a2b
Flanimations <125620313+Flanimations@users.noreply.github.com>
2024-06-03 11:19:51 -0400

    Add files via upload

---
+++ blackcomb 3d BFDI website.js
@@ -0,0 +1,626 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"blackcomb 3d BFDI website_atlas_1", frames: [[0,0,155,171],[157,0,155,171],[314,0,155,171],[471,0,155,171],[628,0,155,171],[785,0,155,171],[942,0,155,171],[1099,0,155,171],[1256,0,155,171],[1413,0,155,171],[1570,0,155,171],[1727,0,155,171],[1884,0,155,171],[0,173,155,171],[157,173,155,171],[314,173,155,171],[471,173,155,171],[628,173,155,171],[785,173,155,171],[942,173,155,171],[1099,173,155,171],[1256,173,155,171],[1413,173,155,171],[1570,173,155,171],[1727,173,155,171],[1884,173,155,171],[0,346,155,171],[157,346,155,171],[314,346,155,171],[471,346,155,171],[628,346,155,171],[785,346,155,171],[942,346,155,171],[1099,346,155,171],[1256,346,155,171],[1413,346,155,171],[1570,346,155,171],[1727,346,155,171],[1884,346,155,171],[0,519,155,171],[157,519,155,171],[314,519,155,171],[471,519,155,171],[628,519,155,171],[785,519,155,171],[942,519,155,171],[1099,519,155,171],[1256,519,155,171],[1413,519,155,171]]}
+];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib._3DBFDI0001 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0002 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0003 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0004 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0005 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0006 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0007 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0008 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(7);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0009 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(8);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0010 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(9);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0011 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(10);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0012 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(11);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0013 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(12);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0014 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(13);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0015 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(14);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0016 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(15);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0017 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(16);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0018 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(17);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0019 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(18);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0020 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(19);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0021 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(20);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0022 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(21);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0023 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(22);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0024 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(23);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0025 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(24);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0026 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(25);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0027 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(26);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0028 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(27);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0029 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(28);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0030 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(29);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0031 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(30);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0032 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(31);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0033 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(32);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0034 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(33);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0035 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(34);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0036 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(35);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0037 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(36);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0038 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(37);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0039 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(38);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0040 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(39);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0041 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(40);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0042 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(41);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0043 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(42);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0044 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(43);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0045 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(44);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0046 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(45);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0047 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(46);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0048 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(47);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib._3DBFDI0049 = function() {
+	this.initialize(ss["blackcomb 3d BFDI website_atlas_1"]);
+	this.gotoAndStop(48);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Symbol1 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib._3DBFDI0049();
+
+	this.instance_1 = new lib._3DBFDI0001();
+
+	this.instance_2 = new lib._3DBFDI0002();
+
+	this.instance_3 = new lib._3DBFDI0003();
+
+	this.instance_4 = new lib._3DBFDI0004();
+
+	this.instance_5 = new lib._3DBFDI0005();
+
+	this.instance_6 = new lib._3DBFDI0006();
+
+	this.instance_7 = new lib._3DBFDI0007();
+
+	this.instance_8 = new lib._3DBFDI0008();
+
+	this.instance_9 = new lib._3DBFDI0009();
+
+	this.instance_10 = new lib._3DBFDI0010();
+
+	this.instance_11 = new lib._3DBFDI0011();
+
+	this.instance_12 = new lib._3DBFDI0012();
+
+	this.instance_13 = new lib._3DBFDI0013();
+
+	this.instance_14 = new lib._3DBFDI0014();
+
+	this.instance_15 = new lib._3DBFDI0015();
+
+	this.instance_16 = new lib._3DBFDI0016();
+
+	this.instance_17 = new lib._3DBFDI0017();
+
+	this.instance_18 = new lib._3DBFDI0018();
+
+	this.instance_19 = new lib._3DBFDI0019();
+
+	this.instance_20 = new lib._3DBFDI0020();
+
+	this.instance_21 = new lib._3DBFDI0021();
+
+	this.instance_22 = new lib._3DBFDI0022();
+
+	this.instance_23 = new lib._3DBFDI0023();
+
+	this.instance_24 = new lib._3DBFDI0024();
+
+	this.instance_25 = new lib._3DBFDI0025();
+
+	this.instance_26 = new lib._3DBFDI0026();
+
+	this.instance_27 = new lib._3DBFDI0027();
+
+	this.instance_28 = new lib._3DBFDI0028();
+
+	this.instance_29 = new lib._3DBFDI0029();
+
+	this.instance_30 = new lib._3DBFDI0030();
+
+	this.instance_31 = new lib._3DBFDI0031();
+
+	this.instance_32 = new lib._3DBFDI0032();
+
+	this.instance_33 = new lib._3DBFDI0033();
+
+	this.instance_34 = new lib._3DBFDI0034();
+
+	this.instance_35 = new lib._3DBFDI0035();
+
+	this.instance_36 = new lib._3DBFDI0036();
+
+	this.instance_37 = new lib._3DBFDI0037();
+
+	this.instance_38 = new lib._3DBFDI0038();
+
+	this.instance_39 = new lib._3DBFDI0039();
+
+	this.instance_40 = new lib._3DBFDI0040();
+
+	this.instance_41 = new lib._3DBFDI0041();
+
+	this.instance_42 = new lib._3DBFDI0042();
+
+	this.instance_43 = new lib._3DBFDI0043();
+
+	this.instance_44 = new lib._3DBFDI0044();
+
+	this.instance_45 = new lib._3DBFDI0045();
+
+	this.instance_46 = new lib._3DBFDI0046();
+
+	this.instance_47 = new lib._3DBFDI0047();
+
+	this.instance_48 = new lib._3DBFDI0048();
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},1).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_7}]},1).to({state:[{t:this.instance_8}]},1).to({state:[{t:this.instance_9}]},1).to({state:[{t:this.instance_10}]},1).to({state:[{t:this.instance_11}]},1).to({state:[{t:this.instance_12}]},1).to({state:[{t:this.instance_13}]},1).to({state:[{t:this.instance_14}]},1).to({state:[{t:this.instance_15}]},1).to({state:[{t:this.instance_16}]},1).to({state:[{t:this.instance_17}]},1).to({state:[{t:this.instance_18}]},1).to({state:[{t:this.instance_19}]},1).to({state:[{t:this.instance_20}]},1).to({state:[{t:this.instance_21}]},1).to({state:[{t:this.instance_22}]},1).to({state:[{t:this.instance_23}]},1).to({state:[{t:this.instance_24}]},1).to({state:[{t:this.instance_25}]},1).to({state:[{t:this.instance_26}]},1).to({state:[{t:this.instance_27}]},1).to({state:[{t:this.instance_28}]},1).to({state:[{t:this.instance_29}]},1).to({state:[{t:this.instance_30}]},1).to({state:[{t:this.instance_31}]},1).to({state:[{t:this.instance_32}]},1).to({state:[{t:this.instance_33}]},1).to({state:[{t:this.instance_34}]},1).to({state:[{t:this.instance_35}]},1).to({state:[{t:this.instance_36}]},1).to({state:[{t:this.instance_37}]},1).to({state:[{t:this.instance_38}]},1).to({state:[{t:this.instance_39}]},1).to({state:[{t:this.instance_40}]},1).to({state:[{t:this.instance_41}]},1).to({state:[{t:this.instance_42}]},1).to({state:[{t:this.instance_43}]},1).to({state:[{t:this.instance_44}]},1).to({state:[{t:this.instance_45}]},1).to({state:[{t:this.instance_46}]},1).to({state:[{t:this.instance_47}]},1).to({state:[{t:this.instance_48}]},1).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,155,171);
+
+
+// stage content:
+(lib.blackcomb3dBFDIwebsite = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.instance = new lib.Symbol1("synched",0);
+	this.instance.setTransform(645.5,404.5,2.0083,2.0083,0,0,0,77.7,85.6);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(49));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(1129.5,592.6,-328.70000000000005,-16.600000000000023);
+// library properties:
+lib.properties = {
+	id: '9D0A60F72EFF23438AE71A0C435B6A88',
+	width: 1280,
+	height: 720,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/blackcomb 3d BFDI website_atlas_1.png", id:"blackcomb 3d BFDI website_atlas_1"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['9D0A60F72EFF23438AE71A0C435B6A88'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
